81f015194c111e3cc5c0447354697ab93ea370c8
Omar Ayman <se.omarayman@gmail.com>
2023-02-05 19:20:37 +0200

    switched to vite and added board image

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { type ReactElement } from "react";
+import "./App.css";
+import Board from "./components/Board/Board";
+
+const App: React.FC = (): ReactElement => {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,6 @@
+import "./Board.css";
+const Board: React.FC = () => {
+  return <div className="fill-window"></div>;
+};
+
+export default Board;

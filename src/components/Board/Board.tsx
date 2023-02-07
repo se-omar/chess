b59e3c5783a2e0e73828723ecbbca4a02eb0ec86
Omar Ayman <se.omarayman@gmail.com>
2023-02-07 17:54:25 +0200

    added dragStart and dragEnd functions

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,11 +1,11 @@
 import Piece from "../Piece/Piece";
-
 import "./Board.css";
 import whiteKing from "../../assets/white-pieces/white-king.png";
+
 const Board: React.FC = () => {
   return (
     <div className="fill-window">
-      <Piece image={whiteKing} team="white" />
+      <Piece image={whiteKing} team="white" position={{ x: 5, y: 6 }} />
     </div>
   );
 };
--- src/components/Piece/Piece.tsx
+++ src/components/Piece/Piece.tsx
@@ -1,10 +1,45 @@
-interface PieceProps {
+import { useRef, type DragEventHandler, type MouseEventHandler } from "react";
+
+interface Props {
   image: string;
   team: string;
+  position: Position;
 }
 
-const Piece: React.FC<PieceProps> = (props: PieceProps) => {
-  return <img src={props.image} />;
+interface Position {
+  x: number;
+  y: number;
+}
+
+const Piece: React.FC<Props> = (props: Props) => {
+  const dragPiece = useRef<number>(0);
+
+  const dragStart: any = (e: any, position: number) => {
+    dragPiece.current = position;
+    console.log(e.target);
+  };
+
+  const dragEnd: any = (e: any) => {
+    console.log("drag end");
+  };
+
+  // const dragEnter: any = (e: any) => {
+  //   // dragOverItem.current = position;
+  //   console.log("drag enter");
+  // };
+
+  return (
+    <img
+      onDragStart={(e) => dragStart(e)}
+      onDragEnd={(e) => dragEnd(e)}
+      draggable
+      src={props.image}
+    />
+  );
 };
+
+// const onMouseDown: MouseEventHandler<HTMLDivElement> = () => {
+//   console.log("asdf");
+// };
 
 export default Piece;

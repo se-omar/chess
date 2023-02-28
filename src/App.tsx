f14f67de0b48251a0504adcd617811edeceabe5d
Omar Ayman <omar@pop-os.localdomain>
2023-02-28 22:46:53 +0200

    u

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { type ReactElement } from "react";
 import "./App.css";
 import Board from "./components/Board/Board";
 
-
 const App: React.FC = (): ReactElement => {
   return (
     <div className="App">

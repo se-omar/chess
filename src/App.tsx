9a98eb54fece1751b98c3709ca3ce8100126c7c1
Omar Ayman <se.omarayman@gmail.com>
2023-02-28 22:49:35 +0200

    u

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { type ReactElement } from "react";
 import "./App.css";
 import Board from "./components/Board/Board";
 
+
 const App: React.FC = (): ReactElement => {
   return (
     <div className="App">

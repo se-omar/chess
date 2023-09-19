b2f13c4000f722d32e4a732fcc9b0f6f1e763401
Omar Ayman <se.omarayman@gmail.com>
2023-09-19 14:19:31 +0300

    create pawn in separate class to separate logic

--- src/board.js
+++ src/board.js
@@ -1,49 +1,53 @@
+import Pawn from './pieces/Pawn';
+
 export const renderBoard = (element) => {
   const rows = ['1', '2', '3', '4', '5', '6', '7', '8'];
   const cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
   const renderPiece = (id) => {
+    const whitePawn = new Pawn(id[0], id[1], 'white');
+    const blackPawn = new Pawn(id[0], id[1], 'black');
     if (id[1] === '7') {
-      return "<img class='pieces pawn' src='../src/assets/blackPieces/black-pawn.png' />";
+      return blackPawn.render();
     }
     if (id[1] === '2') {
-      return "<img class='pieces pawn' src='../src/assets/whitePieces/white-pawn.png' />";
+      return whitePawn.render();
     }
-    switch (id) {
-      case 'A8':
-      case 'H8':
-        return "<img class='pieces' src='../src/assets/blackPieces/black-rook.png' />";
-      case 'B8':
-      case 'G8':
-        return "<img class='pieces' src='../src/assets/blackPieces/black-knight.png' />";
-      case 'C8':
-      case 'F8':
-        return "<img class='pieces' src='../src/assets/blackPieces/black-bishop.png' />";
-      case 'D8':
-        return "<img class='pieces' src='../src/assets/blackPieces/black-king.png' />";
-      case 'E8':
-        return "<img class='pieces' src='../src/assets/blackPieces/black-queen.png' />";
-
-      //
-      //  white pieces
-      //
-
-      case 'A1':
-      case 'H1':
-        return "<img class='pieces' src='../src/assets/whitePieces/white-rook.png' />";
-      case 'B1':
-      case 'G1':
-        return "<img class='pieces' src='../src/assets/whitePieces/white-knight.png' />";
-      case 'C1':
-      case 'F1':
-        return "<img class='pieces' src='../src/assets/whitePieces/white-bishop.png' />";
-      case 'D1':
-        return "<img class='pieces' src='../src/assets/whitePieces/white-king.png' />";
-      case 'E1':
-        return "<img class='pieces' src='../src/assets/whitePieces/white-queen.png' />";
-
-      default:
-        return '';
-    }
+    // switch (id) {
+    //   case 'A8':
+    //   case 'H8':
+    //     return "<img class='pieces' src='../src/assets/blackPieces/black-rook.png' />";
+    //   case 'B8':
+    //   case 'G8':
+    //     return "<img class='pieces' src='../src/assets/blackPieces/black-knight.png' />";
+    //   case 'C8':
+    //   case 'F8':
+    //     return "<img class='pieces' src='../src/assets/blackPieces/black-bishop.png' />";
+    //   case 'D8':
+    //     return "<img class='pieces' src='../src/assets/blackPieces/black-king.png' />";
+    //   case 'E8':
+    //     return "<img class='pieces' src='../src/assets/blackPieces/black-queen.png' />";
+    //
+    //   //
+    //   //  white pieces
+    //   //
+    //
+    //   case 'A1':
+    //   case 'H1':
+    //     return "<img class='pieces' src='../src/assets/whitePieces/white-rook.png' />";
+    //   case 'B1':
+    //   case 'G1':
+    //     return "<img class='pieces' src='../src/assets/whitePieces/white-knight.png' />";
+    //   case 'C1':
+    //   case 'F1':
+    //     return "<img class='pieces' src='../src/assets/whitePieces/white-bishop.png' />";
+    //   case 'D1':
+    //     return "<img class='pieces' src='../src/assets/whitePieces/white-king.png' />";
+    //   case 'E1':
+    //     return "<img class='pieces' src='../src/assets/whitePieces/white-queen.png' />";
+    //
+    //   default:
+    //     return '';
+    // }
   };
 
   let rowHtml = '';
@@ -52,9 +56,9 @@ export const renderBoard = (element) => {
     rowHtml += '<div class="row">';
     iswhite = !iswhite;
     for (const col of cols) {
-      rowHtml += `<span id="${col}${row}" class=${iswhite ? 'white' : 'black'}>
-      ${renderPiece(col + row)}
-      </span>`;
+      rowHtml += `<span id="${col}${row}" class=${
+        iswhite ? 'white' : 'black'
+      }> </span>`;
       iswhite = !iswhite;
     }
 
@@ -63,9 +67,9 @@ export const renderBoard = (element) => {
 
   element.innerHTML = rowHtml;
 
-  document.querySelectorAll('.pieces').forEach((el) => {
-    el.addEventListener('click', (e) => {
-      console.log('e:', e);
-    });
-  });
+  for (const row of rows.reverse()) {
+    for (const col of cols) {
+      renderPiece(col + row);
+    }
+  }
 };
---
+++ src/pieces/Pawn.js
@@ -0,0 +1,29 @@
+class Pawn {
+  constructor(col, row, color) {
+    this.col = col;
+    this.row = row;
+    this.color = color;
+  }
+
+  render() {
+    const pawn = document.createElement('img');
+    pawn.setAttribute('class', 'pieces pawn');
+    pawn.setAttribute(
+      'src',
+      `../src/assets/${this.color}Pieces/${this.color}-pawn.png`,
+    );
+
+    pawn.click((e) => {
+      console.log('e:', e);
+    });
+    document.querySelector(`#${this.col}${this.row}`).appendChild(pawn);
+  }
+
+  getAvailableMoves() {}
+
+  move() {}
+
+  attack() {}
+}
+
+export default Pawn;

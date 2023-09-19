bc0c91c27822e6afb935bad4e91d26e55d8d7b62
Omar Ayman <se.omarayman@gmail.com>
2023-09-19 15:07:22 +0300

    color available moves on pawn click

--- src/pieces/Pawn.js
+++ src/pieces/Pawn.js
@@ -1,4 +1,8 @@
+import { COLS, ROWS } from '../utils/constants';
+
 class Pawn {
+  moveCount = 0;
+
   constructor(col, row, color) {
     this.col = col;
     this.row = row;
@@ -6,6 +10,7 @@ class Pawn {
   }
 
   render() {
+    console.log('aoeuoae');
     const pawn = document.createElement('img');
     pawn.setAttribute('class', 'pieces pawn');
     pawn.setAttribute(
@@ -13,13 +18,44 @@ class Pawn {
       `../src/assets/${this.color}Pieces/${this.color}-pawn.png`,
     );
 
-    pawn.click((e) => {
-      console.log('e:', e);
+    pawn.addEventListener('click', (e) => {
+      const [availCols, availRows] = this.getAvailableMoves();
+      this.highlightMoves(availCols, availRows);
     });
     document.querySelector(`#${this.col}${this.row}`).appendChild(pawn);
   }
 
-  getAvailableMoves() {}
+  getAvailableMoves() {
+    const availCols = [];
+    const availRows = [];
+    if (this.moveCount === 0) {
+      if (this.color === 'white') {
+        availRows.push(
+          ROWS[ROWS.indexOf(this.row) + 1],
+          ROWS[ROWS.indexOf(this.row) + 2],
+        );
+      } else {
+        availRows.push(
+          ROWS[ROWS.indexOf(this.row) - 1],
+          ROWS[ROWS.indexOf(this.row) - 2],
+        );
+      }
+    }
+
+    return [availCols, availRows];
+  }
+
+  highlightMoves(availCols, availRows) {
+    availCols.forEach((col) => {
+      document
+        .querySelector(`#${col}${this.row}`)
+        .setAttribute('class', 'highlighted');
+    });
+
+    availRows.forEach((row) => {
+      document.querySelector(`#${this.col}${row}`).className += ' highlighted';
+    });
+  }
 
   // move() {}
   //

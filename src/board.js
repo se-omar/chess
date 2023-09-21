a4d1c07e2192a30c9b8542ff1a11cf6fddd17357
Omar Ayman <se.omarayman@gmail.com>
2023-09-21 19:41:41 +0300

    handle pawn move

--- src/board.js
+++ src/board.js
@@ -1,71 +1,84 @@
 import Pawn from './pieces/Pawn';
 import { COLS, ROWS } from './utils/constants';
 
+const handlePieceClick = (piece, element) => {
+  if (!element.classList.contains('clickedPiece')) {
+    element.classList.add('clickedPiece');
+    const [availCols, availRows] = piece.getAvailableMoves();
+    piece.highlightMoves(availCols, availRows);
+  } else {
+    piece.removeHighlight();
+    element.classList.remove('clickedPiece');
+  }
+
+  document.querySelectorAll('.clickedPiece').forEach((el) => {
+    if (el !== element) {
+      el.classList.remove('clickedPiece');
+    }
+  });
+};
+
+const renderPiece = (id) => {
+  const whitePawn = new Pawn(id[0], id[1], 'white');
+  const blackPawn = new Pawn(id[0], id[1], 'black');
+  if (id[1] === '7') {
+    const blackPawnEl = blackPawn.render();
+    blackPawnEl.addEventListener('click', () => {
+      handlePieceClick(blackPawn, blackPawnEl);
+    });
+  }
+  if (id[1] === '2') {
+    const whitePawnEl = whitePawn.render();
+    whitePawnEl.addEventListener('click', () => {
+      handlePieceClick(whitePawn, whitePawnEl);
+    });
+  }
+  // switch (id) {
+  //   case 'A8':
+  //   case 'H8':
+  //     return "<img class='pieces' src='../src/assets/blackPieces/black-rook.png' />";
+  //   case 'B8':
+  //   case 'G8':
+  //     return "<img class='pieces' src='../src/assets/blackPieces/black-knight.png' />";
+  //   case 'C8':
+  //   case 'F8':
+  //     return "<img class='pieces' src='../src/assets/blackPieces/black-bishop.png' />";
+  //   case 'D8':
+  //     return "<img class='pieces' src='../src/assets/blackPieces/black-king.png' />";
+  //   case 'E8':
+  //     return "<img class='pieces' src='../src/assets/blackPieces/black-queen.png' />";
+  //
+  //   //
+  //   //  white pieces
+  //   //
+  //
+  //   case 'A1':
+  //   case 'H1':
+  //     return "<img class='pieces' src='../src/assets/whitePieces/white-rook.png' />";
+  //   case 'B1':
+  //   case 'G1':
+  //     return "<img class='pieces' src='../src/assets/whitePieces/white-knight.png' />";
+  //   case 'C1':
+  //   case 'F1':
+  //     return "<img class='pieces' src='../src/assets/whitePieces/white-bishop.png' />";
+  //   case 'D1':
+  //     return "<img class='pieces' src='../src/assets/whitePieces/white-king.png' />";
+  //   case 'E1':
+  //     return "<img class='pieces' src='../src/assets/whitePieces/white-queen.png' />";
+  //
+  //   default:
+  //     return '';
+  // }
+};
+
+const movePiece = (e) => {
+  if (e.target.classList.contains('highlighted')) {
+    const clickedPiece = document.querySelector('.clickedPiece');
+    e.target.appendChild(clickedPiece);
+  }
+};
+
 export const renderBoard = (element) => {
-  const renderPiece = (id) => {
-    const whitePawn = new Pawn(id[0], id[1], 'white');
-    const blackPawn = new Pawn(id[0], id[1], 'black');
-    if (id[1] === '7') {
-      const blackPawnEl = blackPawn.render();
-
-      blackPawnEl.addEventListener('click', function () {
-        if (!this.classList.contains('clickedPiece')) {
-          this.classList.add('clickedPiece');
-          const [availCols, availRows] = blackPawn.getAvailableMoves();
-          blackPawn.highlightMoves(availCols, availRows);
-        } else {
-          blackPawn.removeHighlight();
-          this.classList.remove('clickedPiece');
-        }
-
-        document.querySelectorAll('.clickedPiece').forEach((el) => {
-          if (el !== this) {
-            el.classList.remove('clickedPiece');
-          }
-        });
-      });
-    }
-    if (id[1] === '2') {
-      whitePawn.render();
-    }
-    // switch (id) {
-    //   case 'A8':
-    //   case 'H8':
-    //     return "<img class='pieces' src='../src/assets/blackPieces/black-rook.png' />";
-    //   case 'B8':
-    //   case 'G8':
-    //     return "<img class='pieces' src='../src/assets/blackPieces/black-knight.png' />";
-    //   case 'C8':
-    //   case 'F8':
-    //     return "<img class='pieces' src='../src/assets/blackPieces/black-bishop.png' />";
-    //   case 'D8':
-    //     return "<img class='pieces' src='../src/assets/blackPieces/black-king.png' />";
-    //   case 'E8':
-    //     return "<img class='pieces' src='../src/assets/blackPieces/black-queen.png' />";
-    //
-    //   //
-    //   //  white pieces
-    //   //
-    //
-    //   case 'A1':
-    //   case 'H1':
-    //     return "<img class='pieces' src='../src/assets/whitePieces/white-rook.png' />";
-    //   case 'B1':
-    //   case 'G1':
-    //     return "<img class='pieces' src='../src/assets/whitePieces/white-knight.png' />";
-    //   case 'C1':
-    //   case 'F1':
-    //     return "<img class='pieces' src='../src/assets/whitePieces/white-bishop.png' />";
-    //   case 'D1':
-    //     return "<img class='pieces' src='../src/assets/whitePieces/white-king.png' />";
-    //   case 'E1':
-    //     return "<img class='pieces' src='../src/assets/whitePieces/white-queen.png' />";
-    //
-    //   default:
-    //     return '';
-    // }
-  };
-
   let rowHtml = '';
   let iswhite = false;
   for (const row of ROWS.reverse()) {
@@ -86,13 +99,9 @@ export const renderBoard = (element) => {
   for (const row of ROWS.reverse()) {
     for (const col of COLS) {
       renderPiece(col + row);
-
-      document.querySelector(`#${col}${row}`).addEventListener('click', (e) => {
-        // if (e.target.classList.contains('highlighted')) {
-        //   const clickedPiece = document.querySelector('.clickedPiece')
-        //
-        // }
-      });
+      document
+        .querySelector(`#${col}${row}`)
+        .addEventListener('click', movePiece);
     }
   }
 };

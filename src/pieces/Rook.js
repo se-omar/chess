fb9e35867175735471ce6b848d30482da8793267
Omar Ayman <se.omarayman@gmail.com>
2023-10-08 17:30:57 +0300

    implement rook getAvailMoves

--- src/pieces/Rook.js
+++ src/pieces/Rook.js
@@ -27,7 +27,23 @@ class Rook {
 
   getAvailAttacks() {}
 
-  getAvailMoves() {}
+  getAvailMoves() {
+    const availPositions = [];
+    const availRows = ROWS.filter((r) => r !== this.position[1]);
+    const availCols = COLS.filter((c) => c !== this.position[0]);
+
+    availRows.forEach((row) => {
+      availPositions.push(this.position[0] + row);
+    });
+
+    availCols.forEach((col) => {
+      availPositions.push(col + this.position[1]);
+    });
+
+    console.log(availPositions);
+
+    return availPositions;
+  }
 }
 
 export default Rook;
